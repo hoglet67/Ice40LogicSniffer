16e01acde95f0c0b65f250a279688ccd6965600f
David Banks <dave@hoglet.com>
2018-02-09 15:13:10 +0000

    TestBench: extend to do a capture

--- src/Logic_Sniffer_tb.v
+++ src/Logic_Sniffer_tb.v
@@ -2,26 +2,48 @@
 
 module Logic_Sniffer_tb();
 
+   parameter BITTIME = (1000000000/115200);
+   parameter BYTETIME = BITTIME * 10;
+
    reg clk;
    reg rstn;
-   
+
    reg rx;
    wire tx;
 
    integer j;
-   
+
    task send_byte;
       input [7:0] byte;
       begin
-         #8680 rx = 0;      
+         #BITTIME rx = 0;
          for (j = 0; j < 8; j = j + 1)
            begin
-              #8680 rx = byte[j];
+              #BITTIME rx = byte[j];
            end
-         #8680 rx = 1;
+         #BITTIME rx = 1;
       end
-   endtask // for
-   
+   endtask
+
+   task send_short;
+      input [7:0] cmd;
+      begin
+         send_byte(cmd);
+      end
+   endtask
+
+   task send_long;
+      input [7:0] cmd;
+      input [31:0] data;
+      begin
+         send_byte(cmd);
+         send_byte(data[31:24]);
+         send_byte(data[23:16]);
+         send_byte(data[15: 8]);
+         send_byte(data[ 7: 0]);
+      end
+   endtask
+
 Logic_Sniffer
    DUT
      (
@@ -38,24 +60,42 @@ Logic_Sniffer
       .armLEDnn(),
       .triggerLEDnn()
       );
-   
+
    initial begin
       $dumpvars;
 
       // initialize 100MHz clock
       clk = 1'b0;
       rstn = 1'b1;
-      
+
       rx = 1'b1;
 
       #1000 rstn = 1'b0;
       #1000 rstn = 1'b1;
       #1000 ;
-      
-      send_byte(8'h02);
+
+      send_short(8'h02);
+      #(BYTETIME * 5);
+      send_short(8'h04);
+      #(BYTETIME * 64);
+      send_short(8'h00);
+      send_short(8'h00);
+      send_short(8'h00);
+      send_short(8'h00);
+      send_short(8'h00);
+      send_short(8'h02);
+      #(BYTETIME * 5);
+      send_long(8'hc0, 32'h00000000);
+      send_long(8'hc1, 32'h00000000);
+      send_long(8'hc2, 32'h00000008);
+      send_long(8'hc3, 32'h00000000);
+      send_long(8'h80, 32'h00000000);
+      send_long(8'h81, 32'hff00ff00);
+      send_long(8'h82, 32'h00000000);
+      send_short(8'h01);
 
       #1000000; // 1ms
-      
+
       $finish;
 
    end
